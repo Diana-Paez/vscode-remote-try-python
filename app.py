6ca36ee1d77fa9160e7ef24e580708c6f39971a0
Diana Mercedes Páez    <125220912+Diana-Paez@users.noreply.github.com>
2023-11-18 16:54:44 +0000

    Create a minigame console app with GitHub Copilot

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import random
 
-from flask import Flask
-app = Flask(__name__)
+def play_round():
+    """
+    Plays one round of rock, paper, scissors game.
+    Returns:
+        1 if the user wins,
+        -1 if the user loses,
+        0 if it's a tie.
+    """
+    items = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(items)
+    user_choice = input("Choose rock, paper or scissors: ").lower()
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+    if user_choice not in items:
+        print("Invalid choice.")
+        return 0
+
+    print(f"Computer chose: {computer_choice}")
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+        return 0
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "paper" and computer_choice == "rock") or \
+         (user_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
+        return 1
+    else:
+        print("You lose!")
+        return -1
+
+def play_game():
+    score = 0
+    while True:
+        result = play_round()
+        score += result
+
+        if result != 0:
+            print(f"Your current score: {score}")
+
+        play_again = input("Do you want to play again? (y/n): ").lower()
+        if play_again != 'y':
+            print(f"Final score: {score}")
+            break
+
+if __name__ == "__main__":
+    play_game()
